817b3a36ff8e9cce62bd6300e85035a9640b6610
roman-cash <rkulikov2013@gmail.com>
2022-10-24 11:26:05 +0300

    add: few e2e tests for endpoints

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -16,6 +16,42 @@ describe('Ysers e2e', () => {
 			.send({ email: 'a@a.ru', password: '1' });
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.com', password: '123' });
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.com', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.com', password: '123' });
+
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('a@a.com');
+	});
+
+	it('Info - error', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.com', password: '123' });
+
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
